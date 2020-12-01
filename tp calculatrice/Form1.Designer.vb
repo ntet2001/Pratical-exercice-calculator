aa09d697ed46bbd60aad415be27b18e02c8bf0a7
ntet2001 <manyamaigor2001@gmail.com>
2020-12-01 05:06:19 +0100

    calculator
    this is a caculator done in visual basic language

---
+++ tp calculatrice/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TXT_AFFICHAGE = New System.Windows.Forms.TextBox()
+        Me.BT_1 = New System.Windows.Forms.Button()
+        Me.BT_2 = New System.Windows.Forms.Button()
+        Me.BT_3 = New System.Windows.Forms.Button()
+        Me.BT_4 = New System.Windows.Forms.Button()
+        Me.BT_5 = New System.Windows.Forms.Button()
+        Me.BT_6 = New System.Windows.Forms.Button()
+        Me.BT_7 = New System.Windows.Forms.Button()
+        Me.BT_8 = New System.Windows.Forms.Button()
+        Me.BT_9 = New System.Windows.Forms.Button()
+        Me.BT_0 = New System.Windows.Forms.Button()
+        Me.BT_CANCEL = New System.Windows.Forms.Button()
+        Me.BT_ADDITION = New System.Windows.Forms.Button()
+        Me.BT_SOUSTRACTION = New System.Windows.Forms.Button()
+        Me.BT_MULTIPLICATION = New System.Windows.Forms.Button()
+        Me.BT_DIVISION = New System.Windows.Forms.Button()
+        Me.BT_RESULTAT = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TXT_AFFICHAGE
+        '
+        Me.TXT_AFFICHAGE.Font = New System.Drawing.Font("Roboto", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_AFFICHAGE.Location = New System.Drawing.Point(25, 42)
+        Me.TXT_AFFICHAGE.Multiline = True
+        Me.TXT_AFFICHAGE.Name = "TXT_AFFICHAGE"
+        Me.TXT_AFFICHAGE.Size = New System.Drawing.Size(245, 33)
+        Me.TXT_AFFICHAGE.TabIndex = 0
+        Me.TXT_AFFICHAGE.Text = "0"
+        '
+        'BT_1
+        '
+        Me.BT_1.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_1.Location = New System.Drawing.Point(25, 116)
+        Me.BT_1.Name = "BT_1"
+        Me.BT_1.Size = New System.Drawing.Size(75, 39)
+        Me.BT_1.TabIndex = 1
+        Me.BT_1.Text = "1"
+        Me.BT_1.UseVisualStyleBackColor = True
+        '
+        'BT_2
+        '
+        Me.BT_2.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_2.Location = New System.Drawing.Point(106, 116)
+        Me.BT_2.Name = "BT_2"
+        Me.BT_2.Size = New System.Drawing.Size(75, 39)
+        Me.BT_2.TabIndex = 2
+        Me.BT_2.Text = "2"
+        Me.BT_2.UseVisualStyleBackColor = True
+        '
+        'BT_3
+        '
+        Me.BT_3.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_3.Location = New System.Drawing.Point(187, 116)
+        Me.BT_3.Name = "BT_3"
+        Me.BT_3.Size = New System.Drawing.Size(75, 39)
+        Me.BT_3.TabIndex = 3
+        Me.BT_3.Text = "3"
+        Me.BT_3.UseVisualStyleBackColor = True
+        '
+        'BT_4
+        '
+        Me.BT_4.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_4.Location = New System.Drawing.Point(25, 157)
+        Me.BT_4.Name = "BT_4"
+        Me.BT_4.Size = New System.Drawing.Size(75, 39)
+        Me.BT_4.TabIndex = 4
+        Me.BT_4.Text = "4"
+        Me.BT_4.UseVisualStyleBackColor = True
+        '
+        'BT_5
+        '
+        Me.BT_5.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_5.Location = New System.Drawing.Point(106, 157)
+        Me.BT_5.Name = "BT_5"
+        Me.BT_5.Size = New System.Drawing.Size(75, 39)
+        Me.BT_5.TabIndex = 5
+        Me.BT_5.Text = "5"
+        Me.BT_5.UseVisualStyleBackColor = True
+        '
+        'BT_6
+        '
+        Me.BT_6.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_6.Location = New System.Drawing.Point(187, 157)
+        Me.BT_6.Name = "BT_6"
+        Me.BT_6.Size = New System.Drawing.Size(75, 39)
+        Me.BT_6.TabIndex = 6
+        Me.BT_6.Text = "6"
+        Me.BT_6.UseVisualStyleBackColor = True
+        '
+        'BT_7
+        '
+        Me.BT_7.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_7.Location = New System.Drawing.Point(25, 202)
+        Me.BT_7.Name = "BT_7"
+        Me.BT_7.Size = New System.Drawing.Size(75, 39)
+        Me.BT_7.TabIndex = 7
+        Me.BT_7.Text = "7"
+        Me.BT_7.UseVisualStyleBackColor = True
+        '
+        'BT_8
+        '
+        Me.BT_8.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_8.Location = New System.Drawing.Point(106, 202)
+        Me.BT_8.Name = "BT_8"
+        Me.BT_8.Size = New System.Drawing.Size(75, 39)
+        Me.BT_8.TabIndex = 8
+        Me.BT_8.Text = "8"
+        Me.BT_8.UseVisualStyleBackColor = True
+        '
+        'BT_9
+        '
+        Me.BT_9.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_9.Location = New System.Drawing.Point(187, 202)
+        Me.BT_9.Name = "BT_9"
+        Me.BT_9.Size = New System.Drawing.Size(75, 39)
+        Me.BT_9.TabIndex = 9
+        Me.BT_9.Text = "9"
+        Me.BT_9.UseVisualStyleBackColor = True
+        '
+        'BT_0
+        '
+        Me.BT_0.Font = New System.Drawing.Font("Space Age", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_0.Location = New System.Drawing.Point(106, 245)
+        Me.BT_0.Name = "BT_0"
+        Me.BT_0.Size = New System.Drawing.Size(75, 38)
+        Me.BT_0.TabIndex = 10
+        Me.BT_0.Text = "0"
+        Me.BT_0.UseVisualStyleBackColor = True
+        '
+        'BT_CANCEL
+        '
+        Me.BT_CANCEL.Location = New System.Drawing.Point(282, 42)
+        Me.BT_CANCEL.Name = "BT_CANCEL"
+        Me.BT_CANCEL.Size = New System.Drawing.Size(75, 33)
+        Me.BT_CANCEL.TabIndex = 11
+        Me.BT_CANCEL.Text = "CE"
+        Me.BT_CANCEL.UseVisualStyleBackColor = True
+        '
+        'BT_ADDITION
+        '
+        Me.BT_ADDITION.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_ADDITION.Location = New System.Drawing.Point(282, 115)
+        Me.BT_ADDITION.Name = "BT_ADDITION"
+        Me.BT_ADDITION.Size = New System.Drawing.Size(75, 23)
+        Me.BT_ADDITION.TabIndex = 12
+        Me.BT_ADDITION.Text = "+"
+        Me.BT_ADDITION.UseVisualStyleBackColor = True
+        '
+        'BT_SOUSTRACTION
+        '
+        Me.BT_SOUSTRACTION.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_SOUSTRACTION.Location = New System.Drawing.Point(282, 163)
+        Me.BT_SOUSTRACTION.Name = "BT_SOUSTRACTION"
+        Me.BT_SOUSTRACTION.Size = New System.Drawing.Size(75, 23)
+        Me.BT_SOUSTRACTION.TabIndex = 13
+        Me.BT_SOUSTRACTION.Text = "-"
+        Me.BT_SOUSTRACTION.UseVisualStyleBackColor = True
+        '
+        'BT_MULTIPLICATION
+        '
+        Me.BT_MULTIPLICATION.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_MULTIPLICATION.Location = New System.Drawing.Point(282, 202)
+        Me.BT_MULTIPLICATION.Name = "BT_MULTIPLICATION"
+        Me.BT_MULTIPLICATION.Size = New System.Drawing.Size(75, 23)
+        Me.BT_MULTIPLICATION.TabIndex = 14
+        Me.BT_MULTIPLICATION.Text = "*"
+        Me.BT_MULTIPLICATION.UseVisualStyleBackColor = True
+        '
+        'BT_DIVISION
+        '
+        Me.BT_DIVISION.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_DIVISION.Location = New System.Drawing.Point(282, 244)
+        Me.BT_DIVISION.Name = "BT_DIVISION"
+        Me.BT_DIVISION.Size = New System.Drawing.Size(75, 23)
+        Me.BT_DIVISION.TabIndex = 15
+        Me.BT_DIVISION.Text = "/"
+        Me.BT_DIVISION.UseVisualStyleBackColor = True
+        '
+        'BT_RESULTAT
+        '
+        Me.BT_RESULTAT.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_RESULTAT.Location = New System.Drawing.Point(282, 296)
+        Me.BT_RESULTAT.Name = "BT_RESULTAT"
+        Me.BT_RESULTAT.Size = New System.Drawing.Size(75, 23)
+        Me.BT_RESULTAT.TabIndex = 16
+        Me.BT_RESULTAT.Text = "="
+        Me.BT_RESULTAT.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(365, 322)
+        Me.Controls.Add(Me.BT_RESULTAT)
+        Me.Controls.Add(Me.BT_DIVISION)
+        Me.Controls.Add(Me.BT_MULTIPLICATION)
+        Me.Controls.Add(Me.BT_SOUSTRACTION)
+        Me.Controls.Add(Me.BT_ADDITION)
+        Me.Controls.Add(Me.BT_CANCEL)
+        Me.Controls.Add(Me.BT_0)
+        Me.Controls.Add(Me.BT_9)
+        Me.Controls.Add(Me.BT_8)
+        Me.Controls.Add(Me.BT_7)
+        Me.Controls.Add(Me.BT_6)
+        Me.Controls.Add(Me.BT_5)
+        Me.Controls.Add(Me.BT_4)
+        Me.Controls.Add(Me.BT_3)
+        Me.Controls.Add(Me.BT_2)
+        Me.Controls.Add(Me.BT_1)
+        Me.Controls.Add(Me.TXT_AFFICHAGE)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CALCULATRICE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TXT_AFFICHAGE As TextBox
+    Friend WithEvents BT_1 As Button
+    Friend WithEvents BT_2 As Button
+    Friend WithEvents BT_3 As Button
+    Friend WithEvents BT_4 As Button
+    Friend WithEvents BT_5 As Button
+    Friend WithEvents BT_6 As Button
+    Friend WithEvents BT_7 As Button
+    Friend WithEvents BT_8 As Button
+    Friend WithEvents BT_9 As Button
+    Friend WithEvents BT_0 As Button
+    Friend WithEvents BT_CANCEL As Button
+    Friend WithEvents BT_ADDITION As Button
+    Friend WithEvents BT_SOUSTRACTION As Button
+    Friend WithEvents BT_MULTIPLICATION As Button
+    Friend WithEvents BT_DIVISION As Button
+    Friend WithEvents BT_RESULTAT As Button
+End Class
---
+++ tp calculatrice/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Public operant1 As Integer
+    Public operant2 As Integer
+    Public result As Double
+    Public signe As String
+    Private Sub BT_1_Click(sender As Object, e As EventArgs) Handles BT_1.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "1"
+    End Sub
+
+    Private Sub BT_CANCEL_Click(sender As Object, e As EventArgs) Handles BT_CANCEL.Click
+        TXT_AFFICHAGE.ResetText()
+    End Sub
+
+    Private Sub BT_2_Click(sender As Object, e As EventArgs) Handles BT_2.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "2"
+    End Sub
+
+    Private Sub BT_3_Click(sender As Object, e As EventArgs) Handles BT_3.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "3"
+    End Sub
+
+    Private Sub BT_4_Click(sender As Object, e As EventArgs) Handles BT_4.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "4"
+    End Sub
+
+    Private Sub BT_5_Click(sender As Object, e As EventArgs) Handles BT_5.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "5"
+    End Sub
+
+    Private Sub BT_6_Click(sender As Object, e As EventArgs) Handles BT_6.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "6"
+    End Sub
+
+    Private Sub BT_7_Click(sender As Object, e As EventArgs) Handles BT_7.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "7"
+    End Sub
+
+    Private Sub BT_8_Click(sender As Object, e As EventArgs) Handles BT_8.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "8"
+    End Sub
+
+    Private Sub BT_9_Click(sender As Object, e As EventArgs) Handles BT_9.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "9"
+    End Sub
+
+    Private Sub BT_0_Click(sender As Object, e As EventArgs) Handles BT_0.Click
+        TXT_AFFICHAGE.Text = TXT_AFFICHAGE.Text + "0"
+    End Sub
+
+    Private Sub BT_ADDITION_Click(sender As Object, e As EventArgs) Handles BT_ADDITION.Click
+        operant1 = Integer.Parse(TXT_AFFICHAGE.Text)
+        TXT_AFFICHAGE.Text = ""
+        signe = "+"
+    End Sub
+
+    Private Sub BT_SOUSTRACTION_Click(sender As Object, e As EventArgs) Handles BT_SOUSTRACTION.Click
+        operant1 = Integer.Parse(TXT_AFFICHAGE.Text)
+        TXT_AFFICHAGE.Text = ""
+        signe = "-"
+    End Sub
+
+    Private Sub BT_MULTIPLICATION_Click(sender As Object, e As EventArgs) Handles BT_MULTIPLICATION.Click
+        operant1 = Integer.Parse(TXT_AFFICHAGE.Text)
+        TXT_AFFICHAGE.Text = ""
+        signe = "*"
+    End Sub
+
+    Private Sub BT_DIVISION_Click(sender As Object, e As EventArgs) Handles BT_DIVISION.Click
+        operant1 = Integer.Parse(TXT_AFFICHAGE.Text)
+        TXT_AFFICHAGE.Text = ""
+        signe = "/"
+    End Sub
+
+    Private Sub BT_RESULTAT_Click(sender As Object, e As EventArgs) Handles BT_RESULTAT.Click
+        operant2 = Integer.Parse(TXT_AFFICHAGE.Text)
+        If signe = "+" Then
+            result = operant1 + operant2
+        ElseIf signe = "-" Then
+            result = operant1 - operant2
+        ElseIf signe = "/" Then
+            result = operant1 / operant2
+        Else
+            result = operant1 * operant2
+        End If
+        TXT_AFFICHAGE.Text = result
+    End Sub
+
+
+End Class
